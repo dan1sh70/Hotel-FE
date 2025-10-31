e0d31a9edf19b4896a094abc792a52a8be957361
Danish shafiq <67628974+dan1sh70@users.noreply.github.com>
2025-10-31 19:23:23 +0530

    Update useBookingStore.js

--- src/stores/useBookingStore.js
+++ src/stores/useBookingStore.js
@@ -9,7 +9,7 @@ export const useBookingStore = create((set, get) => ({
     BookNow: async (payload) => {
         set({ loading: true });
         try {
-            const res = await axios.post("http://localhost:5000/api/bookings/book-now", payload);
+            const res = await axios.post("https://hotel-be-n0rh.onrender.com/api/bookings/book-now", payload);
             console.log('Booking response:', res.data);
             
             set(state => ({ 
@@ -27,7 +27,7 @@ export const useBookingStore = create((set, get) => ({
 
     getConfirmation : async(bookingId) => {
         try {
-            const res = await axios.get(`http://localhost:5000/api/bookings/confirm-booking/${bookingId}`)
+            const res = await axios.get(`https://hotel-be-n0rh.onrender.com/api/bookings/confirm-booking/${bookingId}`)
             set({confirmedBooking : res.data});
             console.log(res.data);
         } catch (error) {
